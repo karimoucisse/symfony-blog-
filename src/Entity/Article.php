fdcc5e9baf8b66432daf6b4a1b42603374fdb2d4
karimou cisse <karimou@pylote.io>
2023-01-26 15:39:22 +0100

    [ADD] dashboard package

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -8,7 +8,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
 {
--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 
+// use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
 class Categorie
 {
